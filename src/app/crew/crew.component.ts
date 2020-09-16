4a247cc227a285b3a0f5c66e7ed42fe81d1fffb4
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-09-16 17:20:05 -0500

    crew component add remove and edit complete code

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crew.component.css']
 })
 export class CrewComponent implements OnInit {
+  memberBeingEdited: object = null;
 inCrew: boolean = false;
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
@@ -17,4 +18,33 @@ inCrew: boolean = false;
 
   ngOnInit() {
   }
+  add(addName : string, fstMission : boolean){
+    let errorMsg : string="";
+    //for(let i =0 ;i< this.crew.length; i++){
+      addName= addName.trim();
+      if(addName===""){
+         errorMsg='Enter a valid name!!'
+      }else if(fstMission){
+         this.crew.push({name: addName , firstMission: fstMission });
+      }else {
+          this.crew.push({name: addName , firstMission: fstMission });
+      }
+    //}
+
+    return errorMsg;
+  }
+  remove(member: object){
+    let index =this.crew.indexOf(member)
+    this.crew.splice(index,1)
+  }
+  edit(member : object){
+    this.memberBeingEdited = member;
+  }
+
+  save(names: string, member: object) {
+    member['name'] = names;
+    this.memberBeingEdited = null;
+    }
+
+
 }
